d8c7ac9d6afcb378f4fd4f694aabe5c8ac73cfb1
dccadar <45686757+dccadar@users.noreply.github.com>
2026-03-12 19:31:37 +0100

    Reorder condition to check for 'remote' state

--- src/main.ts
+++ src/main.ts
@@ -792,9 +792,6 @@ class Tapo extends utils.Adapter {
         const deviceId = id.split(".")[2];
         const stateName = id.split(".")[3];
         const command = id.split(".")[4];
-        if (id.split(".")[3] !== "remote") {
-          return;
-        }
         if (stateName === "exclude" && deviceId) {
           const exclude = state.val === true || state.val === "true";
           this.log.info(`Found stateName=excluded with val=${exclude} for device ${deviceId}`);
@@ -816,7 +813,9 @@ class Tapo extends utils.Adapter {
           await this.setStateAsync(id, exclude, true);
           return;
         }
-
+        if (id.split(".")[3] !== "remote") {
+          return;
+        }
         if (command === "Refresh") {
           this.deviceObjects[deviceId]
             .getDeviceInfo(true)
